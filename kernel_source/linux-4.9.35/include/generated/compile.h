26e5714d9ebd826cd5c4a4b51bfce0afec4f4786
Muhammed Unais P <pmsons001@gmail.com>
2019-01-15 13:31:27 +0530

    First commit to front-end branch

---
+++ kernel_source/linux-4.9.35/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 744 */
+/* SMP */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#744 SMP Fri Jan 11 15:10:07 IST 2019"
+#define LINUX_COMPILE_BY "unaisp"
+#define LINUX_COMPILE_HOST "lab-pc"
+#define LINUX_COMPILER "gcc version 5.4.0 20160609 (Ubuntu 5.4.0-6ubuntu1~16.04.10) "
